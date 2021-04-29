5a1f298ca3aab8772b44a519d8b9058756d0d2d3
srad25 <suzanne.radlein@gmail.com>
2021-04-29 04:22:19 -0400

    checked bracket placement

---
+++ seeds/userSeed.js
@@ -0,0 +1,17 @@
+[
+    {
+    "username": "Sean",
+    "email": "sean@gmail.com",
+    "password":"password12345"
+    },
+    {
+    "username": "Holli",
+    "email": "holli@gmail.com",
+    "password":"password12345"
+    },
+    {
+    "username": "Septie",
+    "email": "septi@gmail.com",
+    "password": "password12345"
+    }
+]
